3dce9c4a7948e87825eee2cc55f6f9db031f326a
tianyi <tianyi.zh2020@outlook.com>
2020-08-13 18:28:06 +0800

    login static page (except form)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Admin from './pages/admin/admin'
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div id='app'>
         <Switch>
           <Route path='/login' component={Login}/>
           <Route path='/admin' component={Admin}/>
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './css/login.less'
+import logo from './imgs/logo.png'
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        login
+      <div className='login'>
+        <header>
+          <img src={logo} alt="logo"/>
+          <h1>商品管理系统</h1>
+        </header>
+        <section>
+          <h1>用户登录</h1>
+          以后会加上antd的form组件
+        </section>
       </div>
     );
   }
