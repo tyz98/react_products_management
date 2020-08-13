02e9e668484a89bb0a33c740de14d8589bb7d93f
tianyi <tianyi.zh2020@outlook.com>
2020-08-13 19:18:46 +0800

    login form using antd

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './css/login.less'
 import logo from './imgs/logo.png'
+import { Form, Input, Button} from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 class Login extends Component {
   render() {
+    const onFinish = values => {
+      console.log('Received values of form: ', values);
+    };
     return (
       <div className='login'>
         <header>
@@ -12,7 +17,47 @@ class Login extends Component {
         </header>
         <section>
           <h1>用户登录</h1>
-          以后会加上antd的form组件
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Username!',
+                },
+              ]}
+            >
+              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your Password!',
+                },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
         </section>
       </div>
     );
