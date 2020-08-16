99253517c8b32c0a1b4a62ed754b217122b9209d
tianyi <tianyi.zh2020@outlook.com>
2020-08-17 00:46:03 +0800

    /admin: display or jump to /login

--- src/containers/admin/admin.jsx
+++ src/containers/admin/admin.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux'
-//import {createDemo1Action, createDemo2Action} from '../../redux/actions/login_action'
+import {Redirect} from 'react-router-dom'
+
 class Admin extends Component {
   componentDidMount() {
     console.log(this.props.userInfo);
   }
   render() {
-    const {userInfo} = this.props;
-    return (
-      <div>
-        Hello, {userInfo.user.username}!
-      </div>
-    );
+    const user = JSON.parse(localStorage.getItem('user'));
+    const token = localStorage.getItem('token');
+    if (user && token) {
+      return (
+        <div>
+          Hello, {user.username}!
+        </div>
+      );
+    }
+    //const {userInfo} = this.props;
+    return <Redirect to='/login'/>;
   }
 }
 
