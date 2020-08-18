b36002e617c473879edf50211da5f43e77474577
tianyi <tianyi.zh2020@outlook.com>
2020-08-18 10:16:38 +0800

    header styles

--- src/containers/admin/header/header.jsx
+++ src/containers/admin/header/header.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import {Button} from 'antd'
+import {FullscreenOutlined} from '@ant-design/icons'
 import './css/header.less'
 class Header extends Component {
   render() {
     return (
-      <header className='header'>Header</header>
+      <header className='header'>
+        <div className='header-top'>
+          <Button size='small'><FullscreenOutlined /></Button>
+          <span className='welcome'>欢迎，username</span>
+          <Button type="link">退出</Button>
+        </div>
+        <div className='header-bottom'>
+          <div className="title">柱状图</div>
+          <div className="today">
+            <span className='time'>2020-8-18 09:48:12</span>
+            <img src="http://api.map.baidu.com/images/weather/day/qing.png" alt="天气图标"/>
+            <span className='weather'>晴</span>
+          </div>
+        </div>
+      </header>
     );
   }
 }
