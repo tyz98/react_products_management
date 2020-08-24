da0810672b18d6ff0a0afe17717ad1ad56837764
tianyi <tianyi.zh2020@outlook.com>
2020-08-25 00:10:22 +0800

    add comment

--- src/containers/role/role.jsx
+++ src/containers/role/role.jsx
@@ -21,13 +21,14 @@ class Role extends Component {
     _id:'',
   };
 
-  addFormRef = React.createRef()
+  addFormRef = React.createRef();
 
   componentDidMount() {
-    this.getRoleList();
-    this.setState({menuList:[{title:"全部权限",key:"top",children:menuList}]});
+    this.getRoleList();//请求角色列表，并保存到状态roleList
+    this.setState({menuList:[{title:"全部权限",key:"top",children:menuList}]});//把menuList维护到状态，并在最外层加最高权限
   }
   
+  //请求角色列表，并保存到状态roleList
   getRoleList = async ()=>{
     const {status, data} = await reqRoleList();
     if (status === 0) {
@@ -37,44 +38,51 @@ class Role extends Component {
     }
   }
 
-
+  //添加角色Modal中Form的确认onClick回调
   handleAddOk = () => {
-    this.addFormRef.current.submit();
+    this.addFormRef.current.submit();//提交表单
   };
+  //添加角色的表单验证成功的回调
+  onAddFinish = async (values)=>{
+    const {roleName} = values
+    const response = await reqRoleAdd(roleName);//发送添加角色的请求
+    const {status} = response;
+    if (status === 0) {
+      this.getRoleList();//更新显示的角色列表
+      message.success(`添加角色${roleName}成功`,1)
+      this.setState({visibleAdd:false});//关闭Modal
+    } else {
+      message.error(`添加角色${roleName}失败`,1)
+    }
+  }
 
+  //"设置权限"的onClick回调
+  showAuthModal = (_id)=>{
+    this.setState({visibleAuth: true, _id});//显示Modal,并用正在操作的角色的_id更新状态
+    const role = this.state.roleList.find((item)=>item._id===_id);//从roleList中找到那一个匹配_id的role
+    this.setState({checkedKeys:role.menus});//把menus中的check
+  }
+
+  
+  //设置权限Modal的确认onClick回调
   handleAuthOk = async ()=>{
     const {_id,checkedKeys} = this.state;
-    const response = await reqRoleAuth({_id,menus:checkedKeys,auth_name:this.props.username});
+    const response = await reqRoleAuth({_id,menus:checkedKeys,auth_name:this.props.username});//设置权限的请求
     if (response.status === 0) {
-      this.getRoleList();
+      this.getRoleList();//更新角色列表
       message.success('授权成功',1);
-      this.setState({checkedKeys:[],visibleAuth:false});
+      this.setState({checkedKeys:[],visibleAuth:false});//清除checked，关闭Modal
     } else {
       message.error('授权失败',1);
     }
   }
 
-  onAddFinish = async (values)=>{
-    const {roleName} = values
-    const response = await reqRoleAdd(roleName);
-    const {status} = response;
-    if (status === 0) {
-      this.getRoleList();
-      message.success(`添加角色${roleName}成功`,1)
-      this.setState({visibleAdd:false});
-    } else {
-      message.error(`添加角色${roleName}失败`,1)
-    }
-  }
-  onAddFinishFailed = ()=>{
-    message.error('请检查输入')
-  }
+  //每次check发生变化即更新状态（受控）
+  onCheck = checkedKeys => {
+    //console.log('onCheck', checkedKeys);
+    this.setState({checkedKeys});
+  };
 
-  showAuthModal = (_id)=>{
-    this.setState({visibleAuth: true, _id});
-    const role = this.state.roleList.find((item)=>item._id===_id);
-    this.setState({checkedKeys:role.menus})
-  }
   render() {
     const {roleList,checkedKeys,visibleAdd,visibleAuth,menuList} = this.state;
     console.log('roleList',roleList)
@@ -105,16 +113,11 @@ class Role extends Component {
         title: '操作',
         key:'operation',
         render:(item)=>{
-          return <Button type='link' onClick={() => {this.showAuthModal(item._id)}}>设置权限</Button>
+          return <Button type='link' onClick={() => {this.showAuthModal(item._id)}}>设置权限</Button>//onclick要传入当前操作的id
         },
         align:'center',
       }
     ];
-      
-      const onCheck = checkedKeys => {
-        console.log('onCheck', checkedKeys);
-        this.setState({checkedKeys});
-      };
     
     return (
       <div>
@@ -137,7 +140,8 @@ class Role extends Component {
           onCancel={() => {this.setState({visibleAdd: false})}}
           forceRender
         >
-          <Form ref={this.addFormRef} onFinish={this.onAddFinish} onFinishFailed={this.onAddFinishFailed}>
+          {/* 需要addFormRef来提交表单 */}
+          <Form ref={this.addFormRef} onFinish={this.onAddFinish} onFinishFailed={()=>{message.error('请检查输入',1)}}>
             <Item name='roleName' rules={[{required:true, message:"角色名不能为空！"}]}>
               <Input placeholder='请输入角色名' ></Input>
             </Item>
@@ -152,10 +156,10 @@ class Role extends Component {
         >
           <Tree
             checkable
-            onCheck={onCheck}
+            onCheck={this.onCheck}
             checkedKeys={checkedKeys}
             treeData={menuList}
-            defaultExpandAll
+            defaultExpandAll//默认打开所有父节点
           />
         </Modal>
       </div>
